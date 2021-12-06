ad8ad809f646c6e5ec884ebd092ef3c94d0bd4a8
Daniel Zhang <dzhangwh@gmail.com>
2021-12-06 00:14:42 -0500

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+    }
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+    }
+
+ function destructivelyRemoveLastCat() {
+    cats.pop();
+    }    
+    
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    }    
+
+function appendCat(copyCatsOne) {
+    copyCatsOne = [...cats, "Broom"];
+    return copyCatsOne;
+    }    
+
+function prependCat(copyCatsTwo) {
+    copyCatsTwo  = ["Arnold", ...cats];
+    return copyCatsTwo;
+    }    
+
+function removeLastCat(copyCatsThree) {
+    copyCatsThree  = cats.slice(0, cats.length -1);
+    return copyCatsThree;
+    }    
+
+function removeFirstCat(copyCatsFour) {
+    copyCatsFour  = cats.slice(1);
+    return copyCatsFour;
+    }  
